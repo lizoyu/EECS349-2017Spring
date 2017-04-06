b4ed8cd28f643bd7adcdf71acd6439508173a5a4
Chungyuen Li <lizoyu1@gmail.com>
2017-04-06 11:38:14 -0500

    node initial commit

--- PS2/node.py
+++ PS2/node.py
@@ -1,5 +1,20 @@
 class Node:
     def __init__(self):
+    	# label: name of attribute for splitting
         self.label = None
         self.children = {}
 	# you may want to add additional fields here...
+
+	def set_label(self, label):
+		self.label = label
+
+	def get_label(self):
+		return self.label
+
+	def get_children(self):
+		return self.children
+
+	# attribute value + child node
+	def add_children(self, attribute_val, child):
+		self.children[attribute_val] = child
+
